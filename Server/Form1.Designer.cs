607a29deb168150b7ef7b98b08421c2c898eb498
Ramez Nasser <rameznasser55@gmail.com>
2024-05-22 16:20:21 +0300

    first commit

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chat_box = new System.Windows.Forms.Label();
+            this.message_box = new System.Windows.Forms.Label();
+            this.chatBox = new System.Windows.Forms.TextBox();
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.send = new System.Windows.Forms.Button();
+            this.listen = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Open = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(487, 264);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(375, 194);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chat_box
+            // 
+            this.chat_box.AutoSize = true;
+            this.chat_box.Location = new System.Drawing.Point(484, 112);
+            this.chat_box.Name = "chat_box";
+            this.chat_box.Size = new System.Drawing.Size(50, 13);
+            this.chat_box.TabIndex = 13;
+            this.chat_box.Text = "Chat Box";
+            // 
+            // message_box
+            // 
+            this.message_box.AutoSize = true;
+            this.message_box.Location = new System.Drawing.Point(484, 13);
+            this.message_box.Name = "message_box";
+            this.message_box.Size = new System.Drawing.Size(71, 13);
+            this.message_box.TabIndex = 12;
+            this.message_box.Text = "Message Box";
+            // 
+            // chatBox
+            // 
+            this.chatBox.Location = new System.Drawing.Point(487, 128);
+            this.chatBox.Multiline = true;
+            this.chatBox.Name = "chatBox";
+            this.chatBox.Size = new System.Drawing.Size(311, 116);
+            this.chatBox.TabIndex = 11;
+            // 
+            // messageBox
+            // 
+            this.messageBox.Location = new System.Drawing.Point(487, 29);
+            this.messageBox.Multiline = true;
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(311, 66);
+            this.messageBox.TabIndex = 10;
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(842, 27);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(75, 23);
+            this.send.TabIndex = 9;
+            this.send.Text = "Send";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // listen
+            // 
+            this.listen.Location = new System.Drawing.Point(101, 29);
+            this.listen.Name = "listen";
+            this.listen.Size = new System.Drawing.Size(75, 23);
+            this.listen.TabIndex = 8;
+            this.listen.Text = "Listen";
+            this.listen.UseVisualStyleBackColor = true;
+            this.listen.Click += new System.EventHandler(this.listen_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(26, 58);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(392, 311);
+            this.axWindowsMediaPlayer1.TabIndex = 15;
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(842, 221);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 16;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 495);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chat_box);
+            this.Controls.Add(this.message_box);
+            this.Controls.Add(this.chatBox);
+            this.Controls.Add(this.messageBox);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.listen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label chat_box;
+        private System.Windows.Forms.Label message_box;
+        private System.Windows.Forms.TextBox chatBox;
+        private System.Windows.Forms.TextBox messageBox;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.Button listen;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button Open;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        TcpListener server;
+        TcpClient client;
+        NetworkStream stream;
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        [Obsolete]
+        public Form1() => InitializeComponent();
+
+        [Obsolete]
+        private void listen_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => StartServer());
+        }
+
+        private async Task StartServer()
+        {
+            server = new TcpListener(System.Net.IPAddress.Any, 9050);
+            server.Start();
+            AppendToChatBox("Server started. Waiting for connections...");
+
+            while (true)
+            {
+                client = await server.AcceptTcpClientAsync();
+                AppendToChatBox("Client connected.");
+                _ = Task.Run(() => HandleClientAsync(client));
+            }
+        }
+
+        private async Task HandleClientAsync(TcpClient client)
+        {
+            using (client)
+            {
+                stream = client.GetStream();
+
+                byte[] welcomeMessage = Encoding.ASCII.GetBytes("Welcome to the server!\n");
+                await stream.WriteAsync(welcomeMessage, 0, welcomeMessage.Length);
+
+                byte[] buffer = new byte[1024];
+                while (true)
+                {
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+
+                    string receivedMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    if (receivedMessage.StartsWith("dir:"))
+                    {
+                        await SendFilesDirectoriesAsync(receivedMessage);
+                    }
+                    else if (receivedMessage.StartsWith("file:"))
+                    {
+                        await HandleFileRequestAsync(receivedMessage.Substring(5).Trim());
+                    }
+                    else if (receivedMessage.StartsWith("img:"))
+                    {
+                        await HandleImageRequestAsync(receivedMessage.Substring(4).Trim());
+                    }
+                    else if (receivedMessage.StartsWith("video:"))
+                    {
+                        await HandleVideoRequestAsync(receivedMessage.Substring(6).Trim());
+                    }
+                    else if (receivedMessage.StartsWith("web:"))
+                    {
+                        await HandleWebRequestAsync(receivedMessage.Substring(4).Trim());
+                    }
+                    else
+                    {
+                        AppendToChatBox($"Received: {receivedMessage}");
+                    }
+                }
+
+                AppendToChatBox("Client disconnected.");
+            }
+        }
+
+        private async Task HandleWebRequestAsync(string link)
+        {
+            WebClient wc = new WebClient();
+
+            byte[] data = Encoding.ASCII.GetBytes("WEB:");
+            await stream.WriteAsync(data, 0, data.Length);
+
+            byte[] response = wc.DownloadData(link);
+            byte[] sizeData = BitConverter.GetBytes(response.Length);
+
+            await stream.WriteAsync(sizeData, 0, sizeData.Length);
+            await stream.WriteAsync(response, 0, response.Length);
+            ////////////
+            string filename = "webpage.htm";
+            wc.DownloadFile(link, filename);
+            Console.WriteLine(Encoding.ASCII.GetString(response));
+        }
+
+        private async Task HandleFileRequestAsync(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                byte[] data = Encoding.ASCII.GetBytes("File Content:");
+                await stream.WriteAsync(data, 0, data.Length);
+
+                string compressedFilePath = CompressFile(fileName);
+                byte[] content = File.ReadAllBytes(compressedFilePath);
+
+                byte[] sizeData = BitConverter.GetBytes(content.Length);
+                await stream.WriteAsync(sizeData, 0, sizeData.Length);
+                await stream.WriteAsync(content, 0, content.Length);
+            }
+            else
+            {
+                byte[] data = Encoding.ASCII.GetBytes("File Not Exist");
+                await stream.WriteAsync(data, 0, data.Length);
+            }
+        }
+
+        private async Task HandleImageRequestAsync(string imgName)
+        {
+            byte[] data = Encoding.ASCII.GetBytes("img");
+            await stream.WriteAsync(data, 0, data.Length);
+            if (File.Exists(imgName))
+            {
+                Invoke((Action)(() => pictureBox1.ImageLocation = imgName));
+                string compressedFilePath = CompressFile(imgName);
+                byte[] content = File.ReadAllBytes(compressedFilePath);
+                byte[] sizeData = BitConverter.GetBytes(content.Length);
+
+                await stream.WriteAsync(sizeData, 0, sizeData.Length);
+                await stream.WriteAsync(content, 0, content.Length);
+            }
+            else
+            {
+                data = Encoding.ASCII.GetBytes("File Not Exist");
+                await stream.WriteAsync(data, 0, data.Length);
+            }
+        }
+
+        private async Task HandleVideoRequestAsync(string videoName)
+        {
+            byte[] data = Encoding.ASCII.GetBytes("video");
+            await stream.WriteAsync(data, 0, data.Length);
+            if (File.Exists(videoName))
+            {
+                string compressedFilePath = CompressFile(videoName);
+                byte[] content = File.ReadAllBytes(compressedFilePath);
+                byte[] sizeData = BitConverter.GetBytes(content.Length);
+
+                await stream.WriteAsync(sizeData, 0, sizeData.Length);
+                await stream.WriteAsync(content, 0, content.Length);
+
+                Invoke((Action)(() =>
+                {
+                    axWindowsMediaPlayer1.URL = videoName;
+                }));
+            }
+            else
+            {
+                data = Encoding.ASCII.GetBytes("File Not Exist");
+                await stream.WriteAsync(data, 0, data.Length);
+            }
+        }
+
+        private async Task SendFilesDirectoriesAsync(string receivedMessage)
+        {
+            string directoryName = receivedMessage.Substring(4).Trim();
+            DirectoryInfo directory = new DirectoryInfo(directoryName);
+            if (directory.Exists)
+            {
+                var files = directory.GetFiles();
+                foreach (var file in files)
+                {
+                    byte[] data = Encoding.ASCII.GetBytes("file: " + file + Environment.NewLine);
+                    await stream.WriteAsync(data, 0, data.Length);
+                }
+
+                var folders = directory.GetDirectories();
+                foreach (var folder in folders)
+                {
+                    byte[] data = Encoding.ASCII.GetBytes("folder: " + folder + Environment.NewLine);
+                    await stream.WriteAsync(data, 0, data.Length);
+                }
+            }
+            else
+            {
+                byte[] errorMessage = Encoding.ASCII.GetBytes("Directory not found\n");
+                await stream.WriteAsync(errorMessage, 0, errorMessage.Length);
+            }
+        }
+
+        private async void send_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string message = messageBox.Text;
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                messageBox.Text = string.Empty;
+                await stream.WriteAsync(data, 0, data.Length);
+                AppendToChatBox($"Sent to client: {message}");
+            }
+            catch (Exception ex)
+            {
+                AppendToChatBox($"Error sending data to clients: {ex.Message}");
+            }
+        }
+
+        private string CompressFile(string filePath)
+        {
+            string compressedFilePath = filePath + ".gz";
+            using (FileStream originalFileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            using (FileStream compressedFileStream = new FileStream(compressedFilePath, FileMode.Create, FileAccess.Write))
+            using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+            {
+                originalFileStream.CopyTo(compressionStream);
+            }
+            return compressedFilePath;
+        }
+
+        private void AppendToChatBox(string message)
+        {
+            if (chatBox.InvokeRequired)
+            {
+                chatBox.Invoke(new Action(() => chatBox.AppendText(message + Environment.NewLine)));
+            }
+            else
+            {
+                chatBox.AppendText(message + Environment.NewLine);
+            }
+        }
+
+        private async void Open_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                byte[] data = Encoding.ASCII.GetBytes("img");
+                await stream.WriteAsync(data, 0, data.Length);
+                if (File.Exists(openFileDialog.FileName))
+                {
+                    Invoke((Action)(() => pictureBox1.ImageLocation = openFileDialog.FileName));
+                    string compressedFilePath = CompressFile(openFileDialog.FileName);
+                    byte[] content = File.ReadAllBytes(compressedFilePath);
+                    byte[] sizeData = BitConverter.GetBytes(content.Length);
+
+                    await stream.WriteAsync(sizeData, 0, sizeData.Length);
+                    await stream.WriteAsync(content, 0, content.Length);
+                }
+                else
+                {
+                    data = Encoding.ASCII.GetBytes("File Not Exist");
+                    await stream.WriteAsync(data, 0, data.Length);
+                }
+            }
+        }
+    }
+}
